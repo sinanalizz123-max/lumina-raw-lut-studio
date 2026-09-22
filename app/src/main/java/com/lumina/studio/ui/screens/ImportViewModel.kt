cd8b328ca2f8eb26c213bf812196e09b8a2c7625
Sinanali <sinanalizz123@gmail.com>
2026-09-22 15:51:38 +0530

    fix: close import transaction cleanup scope

--- app/src/main/java/com/lumina/studio/ui/screens/ImportViewModel.kt
+++ app/src/main/java/com/lumina/studio/ui/screens/ImportViewModel.kt
@@ -202,9 +202,13 @@ class ImportViewModel(application: Application) : AndroidViewModel(application) 
             width = bounds.width,
             height = bounds.height
         )
-        database.projectDao().upsert(project)
-        EditHistoryLog.log(database, project.id, EditHistoryLog.IMPORT)
-        return ImportResult.Success(project.id, isRaw, exif, previewOnly, previewNote)
+            database.projectDao().upsert(project)
+            EditHistoryLog.log(database, project.id, EditHistoryLog.IMPORT)
+            return ImportResult.Success(project.id, isRaw, exif, previewOnly, previewNote)
+        } catch (t: Throwable) {
+            runCatching { ProjectStore.deleteProjectFiles(context, projectId) }
+            throw t
+        }
     }
 
     private fun probeDngDevelopability(file: File): Pair<Boolean, String>? {
