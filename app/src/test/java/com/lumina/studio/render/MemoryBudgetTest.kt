9fd712e446a94d3ee93bd51d414734b5f9758c50
Sinanali <sinanalizz123@gmail.com>
2026-09-22 15:38:28 +0530

    Test: pin image memory budget behavior

---
+++ app/src/test/java/com/lumina/studio/render/MemoryBudgetTest.kt
@@ -0,0 +1,19 @@
+package com.lumina.studio.render
+
+import com.lumina.studio.core.render.MemoryBudget
+import org.junit.Assert.assertFalse
+import org.junit.Assert.assertTrue
+import org.junit.Test
+
+class MemoryBudgetTest {
+    @Test
+    fun tiffBudgetRejectsOverflowAndHugeFrames() {
+        assertTrue(MemoryBudget.exceedsTiffBudget(1, 1, 1))
+        assertTrue(MemoryBudget.exceedsTiffBudget(Int.MAX_VALUE, Int.MAX_VALUE, Long.MAX_VALUE))
+    }
+
+    @Test
+    fun tiffBudgetAllowsTinyFrameWithReasonableCap() {
+        assertFalse(MemoryBudget.exceedsTiffBudget(16, 16, 16L * 16L * 20L))
+    }
+}
